fcf452aa1dc4a69d351388b305f362fe62158974
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2019-10-01 10:56:12 +0200

    Update coco_utils.py

--- references/detection/coco_utils.py
+++ references/detection/coco_utils.py
@@ -240,10 +240,11 @@ def get_coco(root, image_set, transforms, mode='instances'):
     print(img_folder, ann_file)
 
     dataset = CocoDetection(img_folder, ann_file, transforms=transforms)
+    print('length before: ', len(dataset))
 
     if image_set == "train":
         dataset = _coco_remove_images_without_annotations(dataset)
-
+    print('length after: ', len(dataset))
     # dataset = torch.utils.data.Subset(dataset, [i for i in range(500)])
 
     return dataset
