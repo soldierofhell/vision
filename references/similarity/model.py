8cf6ef0bdcbba43884c7317bab97523419747744
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-01-15 16:03:48 +0100

    Update model.py

--- references/similarity/model.py
+++ references/similarity/model.py
@@ -7,7 +7,7 @@ class EmbeddingNet(nn.Module):
     def __init__(self, backbone=None):
         super(EmbeddingNet, self).__init__()
         if backbone is None:
-            backbone = models.resnet50(num_classes=128)
+            backbone = models.resnet101(num_classes=128)
 
         self.backbone = backbone
 
