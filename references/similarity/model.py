322b6ea366f23638ef87356106e76c8a8726b2b8
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-02-12 14:55:42 +0100

    Update model.py

--- references/similarity/model.py
+++ references/similarity/model.py
@@ -7,14 +7,14 @@ class EmbeddingNet(nn.Module):
     def __init__(self, backbone=None):
         super(EmbeddingNet, self).__init__()
         if backbone is None:
-            model = models.resnet.ResNet(models.resnet.Bottleneck, [3, 4, 6, 3], num_classes=128)
+            backbone = models.resnet.ResNet(models.resnet.Bottleneck, [3, 4, 6, 3], num_classes=128)
             state_dict = models.utils.load_state_dict_from_url(models.resnet.model_urls['resnet50'],
                                               progress=True)
             for key in list(state_dict.keys()):
                 if key in ["fc.weight", "fc.bias"]:
                     state_dict.pop(key)
             
-            model.load_state_dict(state_dict, strict=False)
+            backbone.load_state_dict(state_dict, strict=False)
             
             #backbone = models.resnet101(num_classes=128)
 
