0fe708f82da5baef62feb6d5fdbeeed23f5e30e5
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-02-25 10:22:30 +0100

    Update predictor.py

--- references/similarity/predictor.py
+++ references/similarity/predictor.py
@@ -39,3 +39,33 @@ class Predictor:
       # todo: obliczenia wylacznie na cuda
       # 1. preprocessing bez PIL
       # 2. torch.cdist()
+      
+      
+class TensorPredictor:
+
+  def __init__(self, checkpoint_path):
+
+    self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    self.model = EmbeddingNet()
+    self.model.load_state_dict(torch.load(checkpoint_path))
+
+    self.model.to(self.device)
+    self.model.eval()    
+
+  def predict(self, image_list):
+    
+    tensor_list = []
+    
+    for image_tensor in image_list:
+      image_tensor = image_tensor[:,:,::-1]  # BGR -> RBG ?
+      image_tensor = F.interpolate(image_tensor, size=(224, 224))
+      tensor_list.append(image_tensor)
+    
+    input_tensor = torch.stack(tensor_list)
+
+    with torch.no_grad():
+      input_tensor = input_tensor.to(self.device)      
+      embedings = model(input_tensor)
+      
+    return embedings
